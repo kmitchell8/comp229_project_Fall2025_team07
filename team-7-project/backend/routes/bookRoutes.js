e3eed29fcda2b010e5355f702e7ab6a4c3d1b4b2
aalayah905 <aalayah905@gmail.com>
2025-11-10 17:15:17 -0500

    Update bookRoutes.js

--- team-7-project/backend/routes/bookRoutes.js
+++ team-7-project/backend/routes/bookRoutes.js
@@ -1,28 +1,75 @@
 /*
  * File Name: bookRoutes.js
- * Author(s): 
+ * Author(s): Aalayah Rodriguez
  * Student ID (s): 
- * Date: 
+ * Date: nov 10th
  */
 
-const express = require('express');//using type="commonjs"
+const express = require('express');
 const router = express.Router();
 const Book = require('../models/books');
 
 //Create book(s)
 
+router.post('/', async (req, res) => {
+  try {
+    const newBook = new Book(req.body);
+    const savedBook = await newBook.save();
+    res.status(201).json(savedBook);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+});
 
 //Get all books
+
+router.get('/', async (req, res) => {
+  try {
+    const books = await Book.find();
+    res.status(200).json(books);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
 
 
 //Get a single book
 
+router.get('/:id', async (req, res) => {
+  try {
+    const book = await Book.findById(req.params.id);
+    if (!book) return res.status(404).json({ message: 'Book not found' });
+    res.status(200).json(book);
+  } catch (err) {
+    res.status(500).json({ message: 'Invalid Book ID format.' });
+  }
+});
 
 //Update a single book
 
+router.put('/:id', async (req, res) => {
+  try {
+    const updatedBook = await Book.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true
+    });
+    if (!updatedBook) return res.status(404).json({ message: 'Book not found' });
+    res.status(200).json(updatedBook);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
 
 //Remove a single book
 
+router.delete('/', async (req, res) => {
+  try {
+    const result = await Book.deleteMany({});
+    res.status(200).json({ message: `You deleted ${result.deletedCount} book(s)` });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
 
 //Delete all books
 

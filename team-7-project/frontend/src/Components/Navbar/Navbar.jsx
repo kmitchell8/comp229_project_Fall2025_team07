3c6df4371310ded744a342c1037f0c2b3158b8f2
kmitchell8 <kevon_11@hotmail.com>
2025-12-10 00:15:05 -0500

    cleanup

--- team-7-project/frontend/src/Components/Navbar/Navbar.jsx
+++ team-7-project/frontend/src/Components/Navbar/Navbar.jsx
@@ -81,7 +81,7 @@ const Navbar = () => {
                     <li><a href="./">Home</a>
 
                         {pageString}</li>
-                    <li><a href="./Library.html">Library</a></li>
+                    <li><a href="./library.html">Library</a></li>
                     <li><a href="./services.html">Services</a></li>
                     <li><a href="./contact.html">Contact</a></li>
                     <li><a href="./about.html">About</a></li>
